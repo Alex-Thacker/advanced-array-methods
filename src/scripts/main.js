d00456e2294eadc7601bab8ec88e8acad6dcd261
Alex Thacker <thacker.alex@yahoo.com>
2019-03-20 11:16:33 -0500

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -81,16 +81,65 @@ const agents = businesses.map(business => {
 
 )
 
-console.log(agents)
+// console.log(agents)
 
-agents.forEach(agent => {
-    outEl.innerHTML += `<h2>${agent.fullName}</h2>
-                        <section>${agent.company}<br>
-                        ${agent.phoneNumber}
-                        </section>`;
-    outEl.innerHTML += "<hr/>";
-});
+// agents.forEach(agent => {
+//     outEl.innerHTML += `<h2>${agent.fullName}</h2>
+//                         <section>${agent.company}<br>
+//                         ${agent.phoneNumber}
+//                         </section>`;
+//     outEl.innerHTML += "<hr/>";
+// });
 
 
 
 // Instead of just returning the purchasing agent object, return a new object that has the full name of the purchasing agent, the company name, and the phone number. The data structure is shown below. Use that new data structure to display the agent with their company and phone number
+
+
+
+// document
+//     .querySelector("#companySearch")
+//     .addEventListener("keypress", keyPressEvent => {
+//         if (keyPressEvent.charCode === 13) {
+//             /* WHEN  USER PRESSES ENTER, FIND MATCHING BUSINESS */
+//             const foundBusiness = businesses.find(
+//                 business => {
+//                    return business.orders.includes(keyPressEvent.target.value)
+//                 });
+
+//             outEl.innerHTML = `
+//                 <h2>
+//                 ${foundBusiness.companyName}
+//                 </h2>
+//                 <section>
+//                 ${foundBusiness.addressFullStreet}
+
+//                 </section>
+//                 <section>
+//                 ${foundBusiness.addressCity},
+//                 ${foundBusiness.addressStateCode}
+//                 ${foundBusiness.addressZipCode}
+//                 </section>
+//             `;
+//         }
+//     });
+
+
+
+
+const monthlyRainfall = [23, 13, 27, 20, 20, 31, 33, 26, 19, 12, 14, 12, 10]
+
+const totalRainfall = monthlyRainfall.reduce((x, y) => {
+    return (x/y) * y
+})
+
+console.log(totalRainfall)
+
+
+const words = ["The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog"]
+
+const sentence = words.reduce((x, y) => {
+    return x + " " + y
+})
+
+console.log(sentence)
